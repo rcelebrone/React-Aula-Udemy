48bf38a4f5193ec564d5dda6327371cb49962b6f
rcelebrone <rcelebrone@gmail.com>
2018-02-01 20:58:38 -0200

    inicio do projeto

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_module/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+
+}
+
+
+/*para gerar packages.json:
+
+npm i --save-dev react@15.4.2 react-dom@15.4.2 babel-core@6.22.1 babel-loader@6.2.10 babel-preset-es2015@6.22.0 babel-preset-react@6.22.0 webpack@1.14.0 webpack-dev-server@1.16.2
+*/
